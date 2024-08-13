b151121d4d3940dabf8b6b77b500b82a5404301c
pedrocmr <pedromooreira@gmail.com>
2024-08-13 20:08:12 -0300

    Inicio do projeto.

---
+++ src/main/java/com/recomendalivrodetails/RecomendaLivroDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.recomendalivrodetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendaLivroDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendaLivroDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recomendalivrodetails/RecomendaLivroDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recomendalivrodetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendaLivroDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
